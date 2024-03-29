c33b0f4027bced07effe30b0ce745722be3990f2
leePettigrew <150901334+leePettigrew@users.noreply.github.com>
2024-03-29 03:42:06 +0000

    Added basic api endpoints for register on port 3500, about to implement MongoDB

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState(''); 
+
+    async function register(ev) {
+        ev.preventDefault();
+        await fetch('http://localhost:3500/register', {
+            method: 'POST' ,
+            body: JSON.stringify({username,password}),
+            headers: {'Content-Type':'application/json'}
+        });
+    }
+
     return(
 
-           <form className="register"> 
+           <form className="register" onSubmit={register}> 
+
             <h1>Register</h1>
-            <input type="text" placeholder="Username"/>
-            <input type="password" placeholder="Password"/>
+
+            <input type="text" 
+            placeholder="Username"
+            value={username}
+            onChange={ev => setUsername(ev.target.value)}
+            />
+
+            <input type="password"
+            placeholder="Password" 
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+            />
+
+
+
             <button>Register</button>
            </form>
       
